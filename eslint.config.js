cd4450dc34587100ab4c84b5901e343fa73d4e00
Денис Воробьев <d.vorobyov@awatera.com>
2025-10-08 12:27:44 +0300

    add sonarjs

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ const js = require('@eslint/js');
 const typescriptParser = require('@typescript-eslint/parser');
 const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
 const prettierPlugin = require('eslint-plugin-prettier');
+const sonarjsPlugin = require('eslint-plugin-sonarjs');
 
 module.exports = [
   // Базовые правила для всех TypeScript файлов
@@ -24,6 +25,7 @@ module.exports = [
     plugins: {
       '@typescript-eslint': typescriptPlugin,
       prettier: prettierPlugin,
+      sonarjs: sonarjsPlugin,
     },
     rules: {
       'prettier/prettier': [
@@ -40,6 +42,11 @@ module.exports = [
       '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'warn',
+      
+      // SonarJS правила
+      ...sonarjsPlugin.configs.recommended.rules,
+      'sonarjs/no-duplicate-string': 'warn',
+      'sonarjs/todo-tag': 'warn',
     },
   },
 
@@ -48,6 +55,10 @@ module.exports = [
     files: ['src/types/**/*.ts'],
     rules: {
       '@typescript-eslint/no-unused-vars': 'off',
+      // Отключаем некоторые SonarJS правила для файлов с типами
+      'sonarjs/no-all-duplicated-branches': 'off',
+      'sonarjs/no-identical-expressions': 'off',
+      'sonarjs/no-identical-functions': 'off',
     },
   },
 
